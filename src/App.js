39814dd797eadd6c5b184bdd10f11544f6764a9b
LYejin <vgg789@naver.com>
2021-08-22 00:55:23 +0900

    make a todos

--- src/App.js
+++ src/App.js
@@ -1,25 +1,90 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useReducer, useState } from "react";
 
+import reducer, {
+  initialState,
+  ADD,
+  DEL,
+  COMPLETE,
+  UNCOMPLETE,
+} from "./reducer";
+
+// useReducer는 component가 엄청난 수의 state를 가지고 있을 때 쓰인다.
 function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const [newToDo, setNewToDo] = useState();
+  const onSubmit = (e) => {
+    e.preventDefault();
+    dispatch({ type: ADD, payload: newToDo });
+    setNewToDo("");
+    // 아무것도 안하겠다는 뜻
+  };
+  const onChange = (e) => {
+    const {
+      target: { value },
+    } = e;
+    setNewToDo(value);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <form onSubmit={onSubmit}>
+        <input
+          value={newToDo}
+          type="text"
+          placeholder="write to do"
+          onChange={onChange}
+        />
+      </form>
+
+      <ul>
+        <h2>To Dos</h2>
+        {state.toDos.map((toDo) => (
+          <li key={toDo.id}>
+            <span>{toDo.text}</span>
+            <span onClick={() => dispatch({ type: DEL, payload: toDo.id })}>
+              ❌
+            </span>
+            <span
+              onClick={() => dispatch({ type: COMPLETE, payload: toDo.id })}
+            >
+              ✅
+            </span>
+          </li>
+        ))}
+      </ul>
+      <ul>
+        {state.completed.length !== 0 && (
+          <>
+            <h2>Completed</h2>
+            {state.completed.map((toDo) => (
+              <li key={toDo.id}>
+                <span>{toDo.text}</span>
+                <span
+                  role="img"
+                  aria-label=""
+                  onClick={() => dispatch({ type: DEL, payload: toDo.id })}
+                >
+                  ❌
+                </span>
+                <span
+                  role="img"
+                  aria-label=""
+                  onClick={() =>
+                    dispatch({ type: UNCOMPLETE, payload: toDo.id })
+                  }
+                >
+                  🙅🏼‍♂️
+                </span>
+              </li>
+            ))}
+          </>
+        )}
+      </ul>
+    </>
   );
 }
+
+// dispatch function이 reducer를 실행하는 것이다.
+// dispatch가 action과 함께 reducer function을 실행
+// uuid : 엄청난 수 랜덤으로 생성
 
 export default App;
---
+++ src/reducer.js
@@ -0,0 +1,46 @@
+import { v4 as uuidv4 } from "uuid";
+
+export const initialState = {
+  toDos: [],
+  completed: [],
+};
+
+export const ADD = "add";
+export const DEL = "del";
+export const COMPLETE = "complete";
+export const UNCOMPLETE = "uncomplete";
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case ADD:
+      return {
+        ...state,
+        toDos: [...state.toDos, { text: action.payload, id: uuidv4() }],
+      };
+    case DEL:
+      return {
+        ...state,
+        toDos: state.toDos.filter((toDo) => toDo.id !== action.payload),
+      };
+    case COMPLETE:
+      const target = state.toDos.find((toDo) => toDo.id === action.payload);
+      return {
+        ...state,
+        toDos: state.toDos.filter((toDo) => toDo.id !== action.payload),
+        completed: [...state.completed, { ...target }],
+      };
+    case UNCOMPLETE:
+      const aTarget = state.completed.find(
+        (toDo) => toDo.id === action.payload
+      );
+      return {
+        ...state,
+        completed: state.completed.filter((toDo) => toDo.id !== action.payload),
+        toDos: [...state.toDos, { ...aTarget }],
+      };
+    default:
+      return;
+  }
+};
+
+export default reducer;
