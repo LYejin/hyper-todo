2ddf10fe50fc6ff4f853be2c06c604ac72c1c297
LYejin <vgg789@naver.com>
2021-08-23 01:03:29 +0900

    todo-list with reducer, context

---
+++ src/components/Add.js
@@ -0,0 +1,29 @@
+import React, { useState } from "react";
+import { ADD } from "../actions";
+import { useDispatch } from "../context";
+
+export default () => {
+  const [newToDo, setNewToDo] = useState("");
+  const dispatch = useDispatch();
+  const onSubmit = (e) => {
+    e.preventDefault();
+    dispatch({ type: ADD, payload: newToDo });
+    setNewToDo("");
+  };
+  const onChange = (e) => {
+    const {
+      target: { value },
+    } = e;
+    setNewToDo(value);
+  };
+  return (
+    <form onSubmit={onSubmit}>
+      <input
+        value={newToDo}
+        type="text"
+        placeholder="Write to do"
+        onChange={onChange}
+      />
+    </form>
+  );
+};
---
+++ src/components/App.js
@@ -0,0 +1,31 @@
+import React from "react";
+import Add from "./Add";
+import List from "./List";
+import { useState } from "../context";
+import ToDo from "./ToDo";
+
+function App() {
+  const { toDos, completed } = useState();
+  return (
+    <>
+      <Add />
+      <List name={"To Do"}>
+        {toDos.map((toDo) => (
+          <ToDo key={toDo.id} id={toDo.id} text={toDo.text} />
+        ))}
+      </List>
+      <List name={completed.length !== 0 ? "Completed" : ""}>
+        {completed.map((toDo) => (
+          <ToDo
+            key={toDo.id}
+            id={toDo.id}
+            text={toDo.text}
+            isCompleted={true}
+          />
+        ))}
+      </List>
+    </>
+  );
+}
+
+export default App;
